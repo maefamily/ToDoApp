cb001a0b4a28cb618752702f80efe9b3d41d4f94
maerean <maerean@gmail.com>
2019-05-24 07:06:52 +0300

    initial commit.

---
+++ ToDoApp/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace ToDoApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.alwaysOnTopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.alwaysOnTopToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(159, 54);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // alwaysOnTopToolStripMenuItem
+            // 
+            this.alwaysOnTopToolStripMenuItem.Name = "alwaysOnTopToolStripMenuItem";
+            this.alwaysOnTopToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.alwaysOnTopToolStripMenuItem.Text = "Always on top";
+            this.alwaysOnTopToolStripMenuItem.Click += new System.EventHandler(this.alwaysOnTopToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(155, 6);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(284, 259);
+            this.treeView1.TabIndex = 1;
+            this.treeView1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.treeView1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 259);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "ToDoApp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem alwaysOnTopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ ToDoApp/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Newtonsoft.Json.Serialization;
+
+namespace ToDoApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void applySettings()
+        {
+            this.TopMost = Properties.Settings.Default.isAlwaysOnTop;
+            if (Properties.Settings.Default.location.X != -1)
+                this.Location = Properties.Settings.Default.location;
+            if (Properties.Settings.Default.size.Width != -1)
+                this.Size = Properties.Settings.Default.size;
+        }
+
+        public void showAppContextMenu(Point whereInWindow)
+        {
+            contextMenuStrip1.Show(PointToScreen(whereInWindow));
+        }
+
+        public void showAppContextMenu(MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+                showAppContextMenu(e.Location);
+        }
+
+        public void saveSize()
+        {
+            Properties.Settings.Default.location = this.Location;
+            Properties.Settings.Default.size = this.Size;
+            Properties.Settings.Default.Save();
+        }
+
+        #region Draggable
+        private const int WM_NCHITTEST = 0x84;
+        
+        private const int HTTRANSPARENT = -0x1;
+        private const int HTCLIENT = 0x1;
+        private const int HTCAPTION = 0x2;
+
+        ///
+        /// Handling the window messages
+        ///
+        protected override void WndProc(ref Message message)
+        {
+            //base.WndProc(ref message);
+            if (message.Msg == WM_NCHITTEST && (int)message.Result == HTCLIENT)
+            {
+                message.Result = (IntPtr)HTCAPTION;
+                return;
+            }
+            base.WndProc(ref message);
+        } 
+        #endregion
+
+        #region Events
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            showAppContextMenu(e);
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            //if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            //    contextMenuStrip1.Show(PointToScreen(e.Location));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            applySettings();
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            saveSize();
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            saveSize();
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+            alwaysOnTopToolStripMenuItem.Checked = Properties.Settings.Default.isAlwaysOnTop;
+        }
+
+        private void alwaysOnTopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.isAlwaysOnTop = !Properties.Settings.Default.isAlwaysOnTop;
+            Properties.Settings.Default.Save();
+            applySettings();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void treeView1_MouseUp(object sender, MouseEventArgs e)
+        {
+            showAppContextMenu(e);
+        }
+        #endregion
+    }
+}
